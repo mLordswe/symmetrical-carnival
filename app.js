0af979115ba6bbb127f8b9e3c03bcfb9471dc2ae
tokojkis <alexander.csore@gmail.com>
2024-09-26 00:14:57 +0200

    functional program, looks crap

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-let plants = 1;
 const waterPerWatering = 50;
 const waterBucket = 250;
 const timesToWater = 4;
 
 plantsNeedWater = () => {
-  wateredPlants = plants * waterPerWatering * timesToWater;
+  let plants = document.getElementById("plants").value; //tar värdet från html input
+  wateredPlants = plants * waterPerWatering * timesToWater; //räknar totala mängden vatten
   console.log(wateredPlants);
-  let bucketAmount = wateredPlants / waterBucket;
+  let bucketAmount = wateredPlants / waterBucket; //tar totala vatten och delar det för att få ut mängd dunkar
   bucketAmount = Math.ceil(bucketAmount);
   console.log(bucketAmount);
+  let fertAmount = plants * waterPerWatering * 1; //gör samma som ovan fast med gödsel
+  let fertBags = fertAmount / waterBucket;
+  fertBags = Math.ceil(fertBags);
+  console.log(plants);
+  document.getElementById(
+    "summa"
+  ).innerHTML = `Du behöver ${bucketAmount} vattendunkar och ${fertBags} gödsel för ${plants} plantor `;
 };
-plantsNeedWater();
+document.getElementById("skicka").addEventListener("click", plantsNeedWater);
